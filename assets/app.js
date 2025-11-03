057e0d615ae145fd8ff9f9b546df6c734aec03ea
hamid-321 <hamidc516@gmail.com>
2025-11-03 13:23:35 +0000

    feat: changes to login page and addition of nav, used TW for styling

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,23 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const iconOpen = document.getElementById('menu-icon-open');
+    const iconClosed = document.getElementById('menu-icon-closed');
+
+    if (menuButton) {
+        menuButton.addEventListener('click', () => {
+            // Toggle the 'hidden' class on the menu
+            mobileMenu.classList.toggle('hidden');
+            
+            // Toggle the icons
+            iconOpen.classList.toggle('hidden');
+            iconOpen.classList.toggle('block');
+            iconClosed.classList.toggle('hidden');
+            iconClosed.classList.toggle('block');
+        });
+    }
+});
